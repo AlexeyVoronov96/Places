f586a3ce94680bf995558806cae7e995d9f84daa
Alexey <alexey.voronov96@gmail.com>
2018-11-03 04:01:42 +0500

    Initial Commit

---
+++ Places/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Places
+//
+//  Created by Алексей Воронов on 03/11/2018.
+//  Copyright © 2018 Алексей Воронов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
